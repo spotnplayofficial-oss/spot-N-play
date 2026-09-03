eea38322a2cabd61d614e7684fbb8303640deeca
Sahil <sahilvrmji@gmail.com>
2026-09-03 14:15:18 +0530

    api key ypdates

--- playnsports/client/src/pages/MapSearch.jsx
+++ playnsports/client/src/pages/MapSearch.jsx
@@ -652,12 +652,12 @@ const MapSearch = () => {
                 <RecenterButton position={position} />
 
                 <TileLayer
-                  key={isDark ? 'dark' : 'light'}
-                  url={isDark
-                    ? 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
-                    : 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'}
-                  attribution=""
-                />
+  key={isDark ? 'dark' : 'light'}
+  url={isDark
+    ? `https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png?key=${import.meta.env.VITE_CARTO_API_KEY}`
+    : `https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png?key=${import.meta.env.VITE_CARTO_API_KEY}`}
+  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>'
+/>
 
                 {/* Route */}
                 {routeData && (
